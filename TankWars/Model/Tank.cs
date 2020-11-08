fa3760377400c698e70d9ca3dfea0bd691138565
Preston Powell <u1159141@eng.utah.edu>
2020-11-08 12:13:06 -0700

    Created the model folder with a basic tank class

---
+++ TankWars/Model/Tank.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Model {
+
+    public class Tank {
+
+        private Vector2D
+location { get; set; }
+        private int bodyDirection { get; set; }
+        private int turretDirection { get; set; }
+        private int HP { get; set; }
+
+
+    }
+}
