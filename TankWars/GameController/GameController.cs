0cce2e0f951c46810043e19fec781b643b7cdff8
Preston Powell <u1159141@eng.utah.edu>
2020-11-09 18:07:05 -0700

    Began adding Json deserialization capability

--- TankWars/GameController/GameController.cs
+++ TankWars/GameController/GameController.cs
@@ -5,11 +5,13 @@ using System.Net.Http;
 using System.Net.WebSockets;
 using System.Text.RegularExpressions;
 using NetworkUtil;
+using Newtonsoft.Json;
+using Model;
+using Newtonsoft.Json.Linq;
 
-namespace GameController
-{
-    public class GameController
-    {
+namespace GameController {
+
+    public class GameController {
         // Controller events that the view can subscribe to
         public delegate void MessageHandler(IEnumerable<string> messages);
         public event MessageHandler MessagesArrived;
@@ -26,11 +28,19 @@ namespace GameController
         SocketState theServer = null;
 
         /// <summary>
+        /// Model of the game
+        /// </summary>
+        World world;
+
+        public GameController(int worldSize) {
+            world = new Model.World(worldSize);
+        }
+
+        /// <summary>
         /// Begins the process of connecting to the server
         /// </summary>
         /// <param name="addr"></param>
-        public void Connect(string addr)
-        {
+        public void Connect(string addr) {
             Networking.ConnectToServer(OnConnect, addr, 11000);
         }
 
@@ -39,10 +49,8 @@ namespace GameController
         /// Method to be invoked by the networking library when a connection is made
         /// </summary>
         /// <param name="state"></param>
-        private void OnConnect(SocketState state)
-        {
-            if (state.ErrorOccured)
-            {
+        private void OnConnect(SocketState state) {
+            if (state.ErrorOccured) {
                 // inform the view
                 Error("Error connecting to server");
                 return;
@@ -72,14 +80,13 @@ namespace GameController
         /// data is available
         /// </summary>
         /// <param name="state"></param>
-        private void ReceiveMessage(SocketState state)
-        {
-            if (state.ErrorOccured)
-            {
+        private void ReceiveMessage(SocketState state) {
+            if (state.ErrorOccured) {
                 // inform the view
                 Error("Lost connection to server");
                 return;
             }
+
             ProcessMessages(state);
 
             // Continue the event loop
@@ -94,8 +101,7 @@ namespace GameController
         /// Then inform the view
         /// </summary>
         /// <param name="state"></param>
-        private void ProcessMessages(SocketState state)
-        {
+        private void ProcessMessages(SocketState state) {
             string totalData = state.GetData();
             string[] parts = Regex.Split(totalData, @"(?<=[\n])");
 
@@ -104,33 +110,66 @@ namespace GameController
 
             List<string> newMessages = new List<string>();
 
-            foreach (string p in parts)
-            {
+            foreach (string p in parts) {
                 // Ignore empty strings added by the regex splitter
                 if (p.Length == 0)
                     continue;
+
                 // The regex splitter will include the last string even if it doesn't end with a '\n',
                 // So we need to ignore it if this happens. 
                 if (p[p.Length - 1] != '\n')
                     break;
 
                 // build a list of messages to send to the view
-                newMessages.Add(p);
+                /*Messages.Add(p);*/
+                parseMessage(p);
 
                 // Then remove it from the SocketState's growable buffer
                 state.RemoveData(0, p.Length);
             }
 
             // inform the view
-            MessagesArrived(newMessages);
+            //MessagesArrived(newMessages);
 
+        }
+
+        /// <summary>
+        /// Will use received data to update the model
+        /// </summary>
+        /// <param name="p"></param>
+        private void parseMessage(string p) {
+
+            JObject obj = JObject.Parse(p);
+            JToken token = obj["fieldName"];
+
+            if (null != token)
+                switch (token.ToString()) {
+
+                    case "tank":
+                        world.setTankData(JsonConvert.DeserializeObject<Tank>(p));
+                        break;
+
+                    case "proj":
+                        break;
+
+                    case "wall":
+                        break;
+
+                    case "beam":
+                        break;
+
+                    case "power":
+                        break;
+                }
+
+            JsonConvert.DeserializeObject<Tank>(p);
+            throw new NotImplementedException();
         }
 
         /// <summary>
         /// Closes the connection with the server
         /// </summary>
-        public void Close()
-        {
+        public void Close() {
             theServer?.TheSocket.Close();
         }
 
@@ -138,8 +177,7 @@ namespace GameController
         /// Send a message to the server
         /// </summary>
         /// <param name="message"></param>
-        public void MessageEntered(string message)
-        {
+        public void MessageEntered(string message) {
             if (theServer != null)
                 Networking.Send(theServer.TheSocket, message + "\n");
         }
