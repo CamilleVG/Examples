c8fc90548d3e72ab62011612ddb81bb3b438499e
Preston Powell <u1159141@eng.utah.edu>
2020-11-09 18:06:44 -0700

    Added a constants class

---
+++ TankWars/Resources/Constants.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Resources {
+    public static class Constants {
+
+        public static int MaxHP = 3;
+
+    }
+}
