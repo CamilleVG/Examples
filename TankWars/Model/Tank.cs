e6fc62e3a37702e33c9f18e2acc491fb91f393f2
Preston Powell <u1159141@eng.utah.edu>
2020-11-09 18:08:05 -0700

    Added additional fields to the tank class and added member variables and a contructor.

--- TankWars/Model/Tank.cs
+++ TankWars/Model/Tank.cs
@@ -1,14 +1,46 @@
 ﻿using System;
+using TankWars;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using Resources;
 
 namespace Model {
 
     public class Tank {
 
-        private TankWars.Vector2D
-location { get; set; }
-        private int bodyDirection { get; set; }
-        private int turretDirection { get; set; }
-        private int HP { get; set; }
+        [JsonProperty(PropertyName = "tank")]
+        private int ID;
+
+        [JsonProperty(PropertyName = "loc")]
+        private Vector2D location;
+
+        [JsonProperty(PropertyName = "bdir")]
+        private Vector2D orientation;
+
+        [JsonProperty(PropertyName = "tdir")]
+        private Vector2D aiming = new Vector2D(0, -1);
+
+        [JsonProperty(PropertyName = "name")]
+        private string name;
+
+        [JsonProperty(PropertyName = "hp")]
+        private int hitPoints = Constants.MaxHP;
+
+        [JsonProperty(PropertyName = "score")]
+        private int score = 0;
+
+        [JsonProperty(PropertyName = "died")]
+        private bool died = false;
+
+        [JsonProperty(PropertyName = "dc")]
+        private bool disconnected = false;
+
+        [JsonProperty(PropertyName = "join")]
+        private bool joined = false;
+
+        public Tank() {
+
+        }
 
 
     }
--- TankWars/Model/World.cs
+++ TankWars/Model/World.cs
@@ -1,10 +1,45 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using TankWars;
 
-namespace Model
-{
-    class World
-    {
+namespace Model {
+
+    public class World {
+
+        public int UniverseSize { get; private set; }
+
+        // Number of frames that must pass before a tank can shoot again
+        public int FramesPerShot { get; private set; }
+
+        // How many ms must pass before a defeated tank can respawn
+        public int RespawnRate { get; private set; }
+
+
+        private Dictionary<int, Tank> Players;
+        private Dictionary<int, Projectiles> Powerups;
+
+
+        public World(int size) {
+            UniverseSize = size;
+
+        }
+
+        //public bool addPlayer(string name) {
+        //    Players.Add(name, new Tank());
+        //    return false;
+
+        //}
+
+        public void setTankData(Tank t) {
+            
+
+        }
+
+
+        public bool removePlayer(string name) {
+            return false;
+
+        }
     }
 }
