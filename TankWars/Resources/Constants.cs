4f93d4c20deeed678d23f5560e22975936418c4f
Preston Powell <u1159141@eng.utah.edu>
2020-11-10 19:28:48 -0700

    Remade the resources folder due to suspected extension issues

--- TankWars/Resources/Constants.cs
+++ TankWars/Resources/Constants.cs
@@ -1,13 +1,10 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Resources {
-    public static class Constants {
+    public class Constants {
 
         public static int MaxHP = 3;
+
 
     }
 }
--- TankWars/Resources/README.txt
+++ TankWars/Resources/README.txt
@@ -1,3 +1,5 @@
-﻿Authors:
-Preston Powell
-Camille Van Ginkel
+﻿Authors
+Preston Powell and Camille Van Ginkel
+Parts of the code taken from Daniel Kopta CS3500 University of Utah Fall 2020 class with permission
+
+We made the README and are not sure what to put in it, yet.
