02446e9b672f951973f5ddaed69cf02a59d39057
CamilleVG <u0953582@eng.utah.edu>
2020-11-15 14:59:35 -0700

    We began writing the OnPaint method.  We now draw the background on each frame.  We have an issue centering the view on the users tank.

--- TankWars/DrawingPanel/DrawingPanel.cs
+++ TankWars/DrawingPanel/DrawingPanel.cs
@@ -8,14 +8,21 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using Model;
+using Resources;
+using GameController;
 
-namespace View {
-    public class DrawingPanel : Panel {
+namespace View
+{
+    public class DrawingPanel : Panel
+    {
         private World theWorld;
+        GameController.GameController controller;
 
-        public DrawingPanel(World w) {
+        public DrawingPanel(GameController.GameController cntlr)
+        {
             DoubleBuffered = true;
-            theWorld = w;
+            theWorld = cntlr.GetWorld();
+            controller = cntlr;
         }
 
         /// <summary>
@@ -24,7 +31,8 @@ namespace View {
         /// <param name="size">The world (and image) size</param>
         /// <param name="w">The worldspace coordinate</param>
         /// <returns></returns>
-        private static int WorldSpaceToImageSpace(int size, double w) {
+        private static int WorldSpaceToImageSpace(int size, double w)
+        {
             return (int)w + size / 2;
         }
 
@@ -43,7 +51,8 @@ namespace View {
         /// <param name="worldY">The Y coordinate of the object in world space</param>
         /// <param name="angle">The orientation of the objec, measured in degrees clockwise from "up"</param>
         /// <param name="drawer">The drawer delegate. After the transformation is applied, the delegate is invoked to draw whatever it wants</param>
-        private void DrawObjectWithTransform(PaintEventArgs e, object o, int worldSize, double worldX, double worldY, double angle, ObjectDrawer drawer) {
+        private void DrawObjectWithTransform(PaintEventArgs e, object o, int worldSize, double worldX, double worldY, double angle, ObjectDrawer drawer)
+        {
             // "push" the current transform
             System.Drawing.Drawing2D.Matrix oldMatrix = e.Graphics.Transform.Clone();
 
@@ -64,14 +73,16 @@ namespace View {
         /// </summary>
         /// <param name="o">The object to draw</param>
         /// <param name="e">The PaintEventArgs to access the graphics</param>
-        private void PlayerDrawer(object o, PaintEventArgs e) {
+        private void PlayerDrawer(object o, PaintEventArgs e)
+        {
             Tank p = o as Tank;
 
             int width = 10;
             int height = 10;
             e.Graphics.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
             using (System.Drawing.SolidBrush blueBrush = new System.Drawing.SolidBrush(System.Drawing.Color.Blue))
-            using (System.Drawing.SolidBrush greenBrush = new System.Drawing.SolidBrush(System.Drawing.Color.Green)) {
+            using (System.Drawing.SolidBrush greenBrush = new System.Drawing.SolidBrush(System.Drawing.Color.Green))
+            {
                 // Rectangles are drawn starting from the top-left corner.
                 // So if we want the rectangle centered on the player's location, we have to offset it
                 // by half its size to the left (-width/2) and up (-height/2)
@@ -91,7 +102,21 @@ namespace View {
         /// </summary>
         /// <param name="o">The object to draw</param>
         /// <param name="e">The PaintEventArgs to access the graphics</param>
-        private void PowerupDrawer(object o, PaintEventArgs e) {
+        private void WallDrawer(object o, PaintEventArgs e)
+        {
+            Image wall = Image.FromFile("..\\..\\..\\Resources\\Images\\WallSprite.png");
+            e.Graphics.DrawImage(wall, -(Constants.WALLWIDTH / 2), -(Constants.WALLWIDTH / 2), Constants.WALLWIDTH, Constants.WALLWIDTH);
+        }
+
+        /// <summary>
+        /// Acts as a drawing delegate for DrawObjectWithTransform
+        /// After performing the necessary transformation (translate/rotate)
+        /// DrawObjectWithTransform will invoke this method
+        /// </summary>
+        /// <param name="o">The object to draw</param>
+        /// <param name="e">The PaintEventArgs to access the graphics</param>
+        private void PowerupDrawer(object o, PaintEventArgs e)
+        {
             Powerup p = o as Powerup;
 
             int width = 8;
@@ -99,7 +124,8 @@ namespace View {
             e.Graphics.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
             using (System.Drawing.SolidBrush redBrush = new System.Drawing.SolidBrush(System.Drawing.Color.Red))
             using (System.Drawing.SolidBrush yellowBrush = new System.Drawing.SolidBrush(System.Drawing.Color.Yellow))
-            using (System.Drawing.SolidBrush blackBrush = new System.Drawing.SolidBrush(System.Drawing.Color.Black)) {
+            using (System.Drawing.SolidBrush blackBrush = new System.Drawing.SolidBrush(System.Drawing.Color.Black))
+            {
                 // Circles are drawn starting from the top-left corner.
                 // So if we want the circle centered on the powerup's location, we have to offset it
                 // by half its size to the left (-width/2) and up (-height/2)
@@ -116,23 +142,88 @@ namespace View {
 
 
         // This method is invoked when the DrawingPanel needs to be re-drawn
-        protected override void OnPaint(PaintEventArgs e) {
-            using (System.Drawing.SolidBrush redBrush = new System.Drawing.SolidBrush(System.Drawing.Color.Red)) {
-                Image background = Image.FromFile("..\\..\\..\\Resources\\Images\\Background.png");
-                e.Graphics.DrawImage(background, 0, 0);
-            }
+        protected override void OnPaint(PaintEventArgs e)
+        {
 
-            Console.WriteLine("Reached line 134 in DP");
+            Image background = Image.FromFile("..\\..\\..\\Resources\\Images\\Background.png");
+            e.Graphics.DrawImage(background, 0, 0);
+
+            Console.WriteLine("Reached line 134 in DP:  Player x position is ");
+            double playerX = controller.GetPlayerX();
+            double playerY = controller.GetPlayerY();
+            
+
+            // calculate view/world size ratio
+            int worldSize = theWorld.UniverseSize;
+            double ratio = (double)Constants.VIEWSIZE / (double)worldSize;
+            int halfSizeScaled = (int)(worldSize / 2.0 * ratio);
+
+            double inverseTranslateX = -WorldSpaceToImageSpace(worldSize, playerX) + halfSizeScaled;
+            double inverseTranslateY = -WorldSpaceToImageSpace(worldSize, playerY) + halfSizeScaled;
+
+            e.Graphics.TranslateTransform((float)inverseTranslateX, (float)inverseTranslateY);
 
             // Draw everything
-            lock (theWorld) {
+            lock (theWorld)
+            {
+                foreach (Wall w in theWorld.Walls.Values)
+                {
+                    if (w.orientation == Constants.HORIZONTAL)
+                    {
+                        double startXVal;
+                        double endXVal;
+                        double yVal = w.FirstPoint.GetY();
+                        if (w.FirstPoint.GetX() < w.SecondPoint.GetX())
+                        {
+                            startXVal = w.FirstPoint.GetX();
+                            endXVal = w.SecondPoint.GetX();
+                        }
+                        else
+                        {
+                            startXVal = w.SecondPoint.GetX();
+                            endXVal = w.FirstPoint.GetX();
+                        }
+                        while (startXVal <= endXVal)
+                        {
+                            DrawObjectWithTransform(e, w, theWorld.UniverseSize, startXVal, yVal, 0, WallDrawer);
+                            startXVal += 50;
+                        }
+                    }
+                    else
+                    {//the wall is Vertical
+                        double xVal = w.FirstPoint.GetX();
+                        double startYVal;
+                        double endYVal;
 
-                foreach (Tank play in theWorld.Players.Values) {
+                        if (w.FirstPoint.GetY() < w.SecondPoint.GetY())
+                        {
+                            startYVal = w.FirstPoint.GetY();
+                            endYVal = w.SecondPoint.GetY();
+                        }
+                        else
+                        {
+                            startYVal = w.SecondPoint.GetY();
+                            endYVal = w.FirstPoint.GetY();
+                        }
+                        while (startYVal <= endYVal)
+                        {
+                            DrawObjectWithTransform(e, w, theWorld.UniverseSize, xVal, startYVal, 0, WallDrawer);
+                            startYVal += 50;
+                        }
+
+                    }
+
+                }
+
+
+                foreach (Tank play in theWorld.Players.Values)
+                {
                     //DrawObjectWithTransform(e, play, theWorld.UniverseSize, play.GetLocation().GetX(), play.GetLocation().GetY(), play.GetOrientation().ToAngle(), PlayerDrawer);
                 }
 
                 // Draw the powerups
-                foreach (Powerup pow in theWorld.Powerups.Values) {
+                foreach (Powerup pow in theWorld.Powerups.Values)
+                {
                     //DrawObjectWithTransform(e, pow, theWorld.UniverseSize, pow.GetLocation().GetX(), pow.GetLocation().GetY(), 0, PowerupDrawer);
                 }
             }
